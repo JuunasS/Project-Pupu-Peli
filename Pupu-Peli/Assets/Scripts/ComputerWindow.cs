d576324b66584d88c11f24eaac5b14ac9887a8e0
JoonasS <62592089+JuunasS@users.noreply.github.com>
2025-02-25 16:54:21 +0200

    Input panel created (WIP)

--- Pupu-Peli/Assets/Scripts/ComputerWindow.cs
+++ Pupu-Peli/Assets/Scripts/ComputerWindow.cs
@@ -4,12 +4,14 @@ using UnityEngine.EventSystems;
 
 public class ComputerWindow : MonoBehaviour, IDragHandler, IPointerDownHandler
 {
-
     Vector3 mouseDragStartPos;
 
     public void OnDrag(PointerEventData eventData)
     {
         transform.position = Input.mousePosition - mouseDragStartPos;
+
+        // This sets the parent object as last sibling so remember to use an empty object for containing panel and it's controller
+        transform.parent.SetAsLastSibling();
     }
 
     public void OnPointerDown(PointerEventData eventData)
---
+++ Pupu-Peli/Assets/Scripts/Matrix game scripts/InputPanel.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class InputPanel : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
