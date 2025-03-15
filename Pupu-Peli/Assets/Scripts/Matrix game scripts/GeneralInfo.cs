ef39343fd1befca9b3b8ff8b8915d470784eaed5
JuunasS <62592089+JuunasS@users.noreply.github.com>
2025-03-15 15:06:24 +0200

    Hardcoded value slider generation

--- Pupu-Peli/Assets/Scripts/Matrix game scripts/GeneralInfo.cs
+++ Pupu-Peli/Assets/Scripts/Matrix game scripts/GeneralInfo.cs
@@ -32,14 +32,25 @@ public class GeneralInfo : MonoBehaviour
 
     public void GenerateValueSliders(IcoListObject icoObj)
     {
+        float sliderObjHeight = valueSliderPrefab.GetComponent<RectTransform>().rect.height;
+
         GameObject newValueSlider = Instantiate(valueSliderPrefab, generalInfoPanel.transform, false);
-        /*
-        newValueSlider.transform.SetParent(generalInfoPanel.transform, true);
-
-        */
-        newValueSlider.transform.position = valueSliderPos1.position;
-
-
+        newValueSlider.transform.localPosition = valueSliderPos1.localPosition;
         newValueSlider.GetComponent<IcoValueSlider>().SetSliderValues("Age", icoObj.icoData.minAge, icoObj.icoData.maxAge, icoObj.icoAge);
+/*
+        newValueSlider.transform.SetParent(generalInfoPanel.transform, true);
+        */
+
+        GameObject newValueSlider2 = Instantiate(valueSliderPrefab, generalInfoPanel.transform, false);
+        newValueSlider2.transform.localPosition = valueSliderPos1.localPosition + new Vector3(0, -sliderObjHeight, 0);
+        newValueSlider2.GetComponent<IcoValueSlider>().SetSliderValues("Weight", (int) icoObj.icoData.minWeight, (int) icoObj.icoData.maxWeight, (int) icoObj.icoWeight);
+
+        GameObject newValueSlider3 = Instantiate(valueSliderPrefab, generalInfoPanel.transform, false);
+        newValueSlider3.transform.localPosition = valueSliderPos1.localPosition + new Vector3(0, -sliderObjHeight *2, 0);
+        newValueSlider3.GetComponent<IcoValueSlider>().SetSliderValues("Height", (int) icoObj.icoData.minHeight, (int) icoObj.icoData.maxHeight, (int) icoObj.icoHeight);
+
+        GameObject newValueSlider4 = Instantiate(valueSliderPrefab, generalInfoPanel.transform, false);
+        newValueSlider4.transform.localPosition = valueSliderPos1.localPosition + new Vector3(0, -sliderObjHeight * 3, 0);
+        newValueSlider4.GetComponent<IcoValueSlider>().SetSliderValues("Productivity", icoObj.icoData.minProductivity, icoObj.icoData.maxProductivity, icoObj.icoProductivity);
     }
 }
