1d20e9b82d6da77d7043f4f18bfb1acda808ac24
JoonasS <62592089+JuunasS@users.noreply.github.com>
2025-03-16 16:46:19 +0200

    Value slider generation duplication bug fix

--- Pupu-Peli/Assets/Scripts/Matrix game scripts/GeneralInfo.cs
+++ Pupu-Peli/Assets/Scripts/Matrix game scripts/GeneralInfo.cs
@@ -1,3 +1,5 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,12 +14,15 @@ public class GeneralInfo : MonoBehaviour
     public GameObject valueSliderPrefab;
     public Transform valueSliderPos1;
 
+    public int sliderPaddingBottom;
+
+    public List<IcoValueSlider> valueSliderList;
 
     public void ActivateInfoPanel(IcoListObject icoListObj)
     {
         generalInfoPanel.SetActive(true);
 
-        if(activeInfoItem != null ) { activeInfoItem.isActive = false; }
+        if (activeInfoItem != null) { activeInfoItem.isActive = false; }
 
         icoListObj.isActive = true;
 
@@ -32,25 +37,39 @@ public class GeneralInfo : MonoBehaviour
 
     public void GenerateValueSliders(IcoListObject icoObj)
     {
-        float sliderObjHeight = valueSliderPrefab.GetComponent<RectTransform>().rect.height;
+        if (valueSliderList == null || valueSliderList.Count == 0)
+        {
+            Debug.Log("Generating new general info slider values");
+            float sliderObjHeight = valueSliderPrefab.GetComponent<RectTransform>().rect.height;
 
-        GameObject newValueSlider = Instantiate(valueSliderPrefab, generalInfoPanel.transform, false);
-        newValueSlider.transform.localPosition = valueSliderPos1.localPosition;
-        newValueSlider.GetComponent<IcoValueSlider>().SetSliderValues("Age", icoObj.icoData.minAge, icoObj.icoData.maxAge, icoObj.icoAge);
-/*
-        newValueSlider.transform.SetParent(generalInfoPanel.transform, true);
-        */
+            GameObject newValueSlider = Instantiate(valueSliderPrefab, generalInfoPanel.transform, false);
+            newValueSlider.transform.localPosition = valueSliderPos1.localPosition;
+            newValueSlider.GetComponent<IcoValueSlider>().SetSliderValues("Age", icoObj.icoData.minAge, icoObj.icoData.maxAge, icoObj.icoAge);
 
-        GameObject newValueSlider2 = Instantiate(valueSliderPrefab, generalInfoPanel.transform, false);
-        newValueSlider2.transform.localPosition = valueSliderPos1.localPosition + new Vector3(0, -sliderObjHeight, 0);
-        newValueSlider2.GetComponent<IcoValueSlider>().SetSliderValues("Weight", (int) icoObj.icoData.minWeight, (int) icoObj.icoData.maxWeight, (int) icoObj.icoWeight);
+            GameObject newValueSlider2 = Instantiate(valueSliderPrefab, generalInfoPanel.transform, false);
+            newValueSlider2.transform.localPosition = valueSliderPos1.localPosition + new Vector3(0, -sliderObjHeight - sliderPaddingBottom, 0);
+            newValueSlider2.GetComponent<IcoValueSlider>().SetSliderValues("Weight", (int)icoObj.icoData.minWeight, (int)icoObj.icoData.maxWeight, (int)icoObj.icoWeight);
 
-        GameObject newValueSlider3 = Instantiate(valueSliderPrefab, generalInfoPanel.transform, false);
-        newValueSlider3.transform.localPosition = valueSliderPos1.localPosition + new Vector3(0, -sliderObjHeight *2, 0);
-        newValueSlider3.GetComponent<IcoValueSlider>().SetSliderValues("Height", (int) icoObj.icoData.minHeight, (int) icoObj.icoData.maxHeight, (int) icoObj.icoHeight);
+            GameObject newValueSlider3 = Instantiate(valueSliderPrefab, generalInfoPanel.transform, false);
+            newValueSlider3.transform.localPosition = valueSliderPos1.localPosition + new Vector3(0, (-sliderObjHeight - sliderPaddingBottom) * 2, 0);
+            newValueSlider3.GetComponent<IcoValueSlider>().SetSliderValues("Height", (int)icoObj.icoData.minHeight, (int)icoObj.icoData.maxHeight, (int)icoObj.icoHeight);
 
-        GameObject newValueSlider4 = Instantiate(valueSliderPrefab, generalInfoPanel.transform, false);
-        newValueSlider4.transform.localPosition = valueSliderPos1.localPosition + new Vector3(0, -sliderObjHeight * 3, 0);
-        newValueSlider4.GetComponent<IcoValueSlider>().SetSliderValues("Productivity", icoObj.icoData.minProductivity, icoObj.icoData.maxProductivity, icoObj.icoProductivity);
+            GameObject newValueSlider4 = Instantiate(valueSliderPrefab, generalInfoPanel.transform, false);
+            newValueSlider4.transform.localPosition = valueSliderPos1.localPosition + new Vector3(0, (-sliderObjHeight - sliderPaddingBottom) * 3, 0);
+            newValueSlider4.GetComponent<IcoValueSlider>().SetSliderValues("Productivity", icoObj.icoData.minProductivity, icoObj.icoData.maxProductivity, icoObj.icoProductivity);
+
+            valueSliderList.Add(newValueSlider.GetComponent<IcoValueSlider>());
+            valueSliderList.Add(newValueSlider2.GetComponent<IcoValueSlider>());
+            valueSliderList.Add(newValueSlider3.GetComponent<IcoValueSlider>());
+            valueSliderList.Add(newValueSlider4.GetComponent<IcoValueSlider>());
+        }
+        else
+        {
+            Debug.Log("Setting new general info slider values");
+            valueSliderList[0].SetSliderValues("Age", icoObj.icoData.minAge, icoObj.icoData.maxAge, icoObj.icoAge);
+            valueSliderList[1].SetSliderValues("Weight", (int)icoObj.icoData.minWeight, (int)icoObj.icoData.maxWeight, (int)icoObj.icoWeight);
+            valueSliderList[2].SetSliderValues("Height", (int)icoObj.icoData.minHeight, (int)icoObj.icoData.maxHeight, (int)icoObj.icoHeight);
+            valueSliderList[3].SetSliderValues("Productivity", icoObj.icoData.minProductivity, icoObj.icoData.maxProductivity, icoObj.icoProductivity);
+        }
     }
 }
