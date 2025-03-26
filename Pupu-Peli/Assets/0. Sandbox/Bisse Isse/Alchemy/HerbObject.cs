7f5dbadf55b6a7f2faf0d9028db1ddcc1624e78d
Isabel Puustelli <isabelpuustelli@gmail.com>
2025-03-26 18:17:42 +0200

    Starting alchemy

---
+++ Pupu-Peli/Assets/0. Sandbox/Bisse Isse/Alchemy/HerbObject.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Herb", menuName = "Scriptable Objects/Herb")]
+public class HerbObject : ScriptableObject
+{
+    public Sprite image;
+    public Sprite reverse;
+}
