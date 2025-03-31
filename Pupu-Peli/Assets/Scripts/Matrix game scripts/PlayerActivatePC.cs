a93edf53ae3da68db418308e5784a30dcd2d1457
JoonasS <62592089+JuunasS@users.noreply.github.com>
2025-03-31 13:58:42 +0300

    Game Exit End Screen (WIP)

--- Pupu-Peli/Assets/Scripts/Matrix game scripts/PlayerActivatePC.cs
+++ Pupu-Peli/Assets/Scripts/Matrix game scripts/PlayerActivatePC.cs
@@ -1,15 +1,24 @@
+using System.Collections;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class PlayerActivatePC : MonoBehaviour
 {
 
     public GameObject MatrixCanvasObject;
+    public int waitBeforeExit;
 
-    private void Update()
+    private async void Update()
     {
         if (MatrixCanvasObject & Input.GetKeyDown(KeyCode.Escape))
         {
             //TODO:  Check if player has completed the taks or timer has run out!!!
+            // Show given score and exit after pressing esc again?
+            // Or exit after a few seconds
+            FindAnyObjectByType<MatrixGameManager>().GameEndOnExit();
+
+            await Task.Delay(waitBeforeExit * 1000); // milliseconds -> seconds
+
             MatrixCanvasObject.SetActive(false);
         }
         else if (Input.GetKeyDown(KeyCode.E))
