c65ee64c21b036ed7d9f86004d0217a401e8b741
JoonasS <62592089+JuunasS@users.noreply.github.com>
2025-04-01 15:54:14 +0300

    Coin Spawner (WIP)

---
+++ Pupu-Peli/Assets/Scripts/CoinSpawner.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class CoinSpawner : MonoBehaviour
+{
+
+    public Transform spawnArea;
+
+    public GameObject coinPrefab;
+
+    public float spawnDelay;
+    public int maxSpawnForce;
+    public int minSpawnForce;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnCoinsTEST());
+    }
+
+    public IEnumerator SpawnCoins(int score)
+    {
+        for (int i = 0; i < score / 50; i++)
+        {
+                Vector3 tempSpawnPosition = spawnArea.localPosition;
+                tempSpawnPosition += new Vector3(Random.Range(-0.4f, 0.4f), Random.Range(0, 0.4f), Random.Range(-0.4f, 0.4f));
+
+                GameObject coin = Instantiate(coinPrefab, tempSpawnPosition, spawnArea.transform.rotation);
+                coin.gameObject.transform.SetParent(spawnArea, false);
+
+                coin.GetComponent<Rigidbody>().AddRelativeForce(Quaternion.Euler(0, 0, 45) * new Vector3(0, Random.Range(minSpawnForce, maxSpawnForce), 0), ForceMode.Impulse);
+
+                yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+
+
+    public IEnumerator SpawnCoinsTEST()
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            Vector3 tempSpawnPosition = spawnArea.localPosition;
+            tempSpawnPosition += new Vector3(Random.Range(-0.4f, 0.4f), Random.Range(0, 0.4f), Random.Range(-0.4f, 0.4f));
+
+            GameObject coin = Instantiate(coinPrefab, tempSpawnPosition, spawnArea.transform.rotation);
+            coin.gameObject.transform.SetParent(spawnArea, false);
+
+            coin.GetComponent<Rigidbody>().AddRelativeForce(Quaternion.Euler(0, 0, 45) * new Vector3(0, Random.Range(minSpawnForce, maxSpawnForce), 0), ForceMode.Impulse);
+
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+
+}
