ebfb448c9ac80e5a688d971c88a691e39ba46b8e
JoonasS <62592089+JuunasS@users.noreply.github.com>
2025-04-03 15:11:58 +0300

    Coin prefab and hierarchy changes

---
+++ Pupu-Peli/Assets/Scripts/Coin.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public int value;
+
+    public AudioSource audioSource;
+    public AudioClip coinHitAudio;
+    public AudioClip coinCollectAudio;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        audioSource.clip = coinHitAudio;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Add player the money, sfx and delete this object
+        Debug.Log("Coin collision!");
+        if (collision.transform.tag == "Player")
+        {
+            Debug.Log("Player collided with coin!");
+            audioSource.clip = coinCollectAudio;
+        }
+        else
+        {
+
+            audioSource.clip = coinHitAudio;
+        }
+        audioSource.Play();
+
+    }
+}
