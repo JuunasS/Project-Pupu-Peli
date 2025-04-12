14cfcd99d8cfae06a6068ada36e44ebc3baf23bc
JuunasS <62592089+JuunasS@users.noreply.github.com>
2025-04-12 18:00:51 +0300

    DayNight testing

--- Pupu-Peli/Assets/Scripts/DayNightCycle/DayNightCycle.cs
+++ Pupu-Peli/Assets/Scripts/DayNightCycle/DayNightCycle.cs
@@ -2,15 +2,42 @@ using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Light sun;
+
+    [SerializeField, Range(0, 24)] public float timeOfDay;
+
+    public float rotationSpeed;
+
+    public Gradient skyColor;
+    public Gradient equatorColor;
+    public Gradient sunColor;
+
+    private void Update()
     {
-        
+        timeOfDay += Time.deltaTime * rotationSpeed;
+        if(timeOfDay > 24) { timeOfDay = 0; }
+
+        UpdateSunRotation();
+        UpdateLighting();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnValidate()
     {
-        
+        UpdateSunRotation();
+        UpdateLighting();
+    }
+
+    public void UpdateSunRotation()
+    {
+        float rotation = Mathf.Lerp(-90, 270, timeOfDay / 24);
+        sun.transform.rotation = Quaternion.Euler(rotation, sun.transform.rotation.y, sun.transform.rotation.z);
+    }
+
+    public void UpdateLighting()
+    {
+        float timeFraction = timeOfDay / 24;
+        RenderSettings.ambientEquatorColor = equatorColor.Evaluate(timeFraction);
+        RenderSettings.ambientSkyColor = equatorColor.Evaluate(timeFraction);
+        sun.color = sunColor.Evaluate(timeFraction);
     }
 }
