99a3238bb3787a6c50faaee1f884dbf4d88ba2cc
Isabel Puustelli <isabelpuustelli@gmail.com>
2025-04-23 10:24:09 +0300

    alchemy fixing

--- Pupu-Peli/Assets/0. Sandbox/Bisse Isse/Alchemy/Alchemy.cs
+++ Pupu-Peli/Assets/0. Sandbox/Bisse Isse/Alchemy/Alchemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -8,11 +9,17 @@ public class Alchemy : MonoBehaviour
     public List<Herb> picks = new();
     public List<Herb> solution = new();
     public List<Image> playerImage = new();
+    public List<Image> solutionImages = new();
     public TextMeshProUGUI text;
+    public TextMeshProUGUI counter;
 
     public GameObject win;
     public GameObject lose;
-    
+
+    private void Start()
+    {
+        drawShape(solution, solutionImages);    
+    }
 
     public bool Check()
     {
@@ -36,33 +43,24 @@ public class Alchemy : MonoBehaviour
         return true;
     }
 
-    public void Display()
+    public void drawShape(List<Herb> toSort, List<Image> images)
     {
         List<Sprite> sorted = new();
 
-        foreach (var herb in picks)
+        foreach (var herb in toSort)
         {
             if (!herb.isReverse)
                 sorted.Add(herb.image);
         }
-        foreach (var herb in picks)
+        foreach (var herb in toSort)
         {
             if (herb.isReverse)
                 sorted.Add(herb.reverse);
         }
         for (int i = 0; i < sorted.Count; i++)
         {
-            playerImage[i].enabled = true;
-            playerImage[i].sprite = sorted[i];
-        }
-
-        if (Check())
-        {
-            win.SetActive(true);
-        }
-        else 
-        {
-            lose.SetActive(true);
+            images[i].enabled = true;
+            images[i].sprite = sorted[i];
         }
     }
 
@@ -100,6 +98,20 @@ public class Alchemy : MonoBehaviour
         {
             image.sprite = null;
             image.enabled = false;
+        }
+        counter.text = (Int32.Parse(counter.text) + 1).ToString();
+    }
+
+    public void playerShape()
+    {
+        drawShape(picks, playerImage);
+        if (Check())
+        {
+            win.SetActive(true);
+        }
+        else
+        {
+            lose.SetActive(true);
         }
     }
 }
--- Pupu-Peli/Assets/0. Sandbox/Bisse Isse/Movement.cs
+++ Pupu-Peli/Assets/0. Sandbox/Bisse Isse/Movement.cs
@@ -27,7 +27,7 @@ public class Movement : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            rb.MovePosition(transform.position + moveDir * Time.deltaTime * speed);
+            rb.MovePosition(transform.position + moveDir.normalized * Time.deltaTime * speed);
         }
     }
 
