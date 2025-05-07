4aa09eadc40d29f3a77e21bc8a2e133f84fcf31d
JoonasS <62592089+JuunasS@users.noreply.github.com>
2025-05-07 16:53:20 +0300

    Coin spawner fixes & inventory system start

---
+++ Pupu-Peli/Assets/Scripts/Player/Inventory.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    // Big item hold position
+    Transform bigItem;
+
+    // Small item hold positions
+    Transform[] smallItems = new Transform[2];
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
