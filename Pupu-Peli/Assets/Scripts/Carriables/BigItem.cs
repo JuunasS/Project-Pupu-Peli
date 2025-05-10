e884bee82883a39590ba679a58bc7e9f2e5c0a33
JuunasS <62592089+JuunasS@users.noreply.github.com>
2025-05-10 15:50:28 +0300

    FloatingText prefab

--- Pupu-Peli/Assets/Scripts/Carriables/BigItem.cs
+++ Pupu-Peli/Assets/Scripts/Carriables/BigItem.cs
@@ -3,4 +3,13 @@ using UnityEngine;
 public class BigItem : MonoBehaviour
 {
     // Parent class for big items
+
+    // Pickup popup
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+
+        }
+    }
 }
