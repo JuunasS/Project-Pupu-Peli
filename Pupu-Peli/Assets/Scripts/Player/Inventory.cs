0b1ae360af2c62ca89780fb49a3b4830012d45e8
JoonasS <62592089+JuunasS@users.noreply.github.com>
2025-05-13 14:11:18 +0300

    Pick up functionality

--- Pupu-Peli/Assets/Scripts/Player/Inventory.cs
+++ Pupu-Peli/Assets/Scripts/Player/Inventory.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
@@ -13,41 +15,60 @@ public class Inventory : MonoBehaviour
     // Small item data and positions
     public Transform rightHand;
     public Transform leftHand;
-    public SmallItem[] smallItems = new SmallItem[2];
+    public List<SmallItem> smallItems;
 
     public GameObject activeInteraction = null;
+
+    private void Update()
+    {
+        // Move keyboard press stuff into the input system?
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            DropItem();
+        }
+    }
 
 
     public void SetBigItem(BigItem obj)
     {
-        if(smallItems.Length > 0) { obj.PickUpFailed(); return; }
+        Debug.Log(smallItems.Count);
+        if (smallItems.Count > 0) { obj.PickUpFailed(); return; }
+        obj.PickUpSuccess();
         // ckeck if player has room for item
+        Debug.Log("Setting big item!");
         bigItem = obj;
         obj.transform.SetParent(bigItemHolder);
         obj.transform.position = bigItemHolder.position;
+        obj.transform.rotation = bigItemHolder.rotation;
+        StartCoroutine(WaitForNextActive());
     }
 
 
     public void SetSmallItem(SmallItem obj)
     {
         // ckeck if player has room for item
-        if(bigItem != null) { obj.PickUpFailed(); return; }
+        if (bigItem != null && smallItems.Count <= 2) { obj.PickUpFailed(); return; }
 
+        obj.PickUpSuccess();
+
+        smallItems.Add(obj);
         if (rightHand.childCount == 0)
         {
-            smallItems[0] = obj;
             obj.transform.SetParent(rightHand);
             obj.transform.position = rightHand.position;
+            obj.transform.rotation = rightHand.rotation;
         }
         else if (leftHand.childCount == 0)
         {
-            smallItems[1] = obj;
-            obj.transform.SetParent(leftHand); 
+            obj.transform.SetParent(leftHand);
             obj.transform.position = leftHand.position;
+            obj.transform.rotation = leftHand.rotation;
         }
-        else {
+        else
+        {
             Debug.Log("Hands full!");
         }
+        StartCoroutine(WaitForNextActive());
     }
 
     public void CheckInteractionDistance(GameObject newObj)
@@ -56,12 +77,24 @@ public class Inventory : MonoBehaviour
         float distance2 = Vector3.Distance(this.transform.position, activeInteraction.transform.position);
         if (distance1 < distance2) { activeInteraction = newObj; }
     }
-    
+
     public void DropItem()
     {
-        if (bigItem)
+        if (bigItem != null)
         {
-            
+            bigItem?.DropItem();
+            bigItem = null;
         }
+        for (int i = smallItems.Count - 1; i >= 0; i--)
+        {
+            smallItems[i]?.DropItem();
+            smallItems.RemoveAt(i);
+        }
+    }
+
+    IEnumerator WaitForNextActive()
+    {
+        yield return new WaitForSeconds(0.1f);
+        activeInteraction = null;
     }
 }
