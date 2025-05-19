7a6cdbcbd31fc91465a0ffedf3c4f249e716e762
JoonasS <62592089+JuunasS@users.noreply.github.com>
2025-05-19 15:03:43 +0300

    Dialogue system stuff

--- Pupu-Peli/Assets/Scripts/DialoguePopup.cs
+++ Pupu-Peli/Assets/Scripts/DialoguePopup.cs
@@ -1,5 +1,7 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class DialoguePopup : MonoBehaviour
 {
@@ -9,6 +11,30 @@ public class DialoguePopup : MonoBehaviour
     public bool dialogueShown = false;
     public float dialogueDuration;
 
+    
+    public Renderer _renderer;
+    public Material[] mats;
+    public Material outline; // The outline material to use in code
+    public string outlineAssetName = "OutlineMaterial (Instance)";
+    public float outlineThickness = 1.13f;
+
+    public  void Start()
+    {
+
+        _renderer = GetComponent<Renderer>();
+        mats = _renderer.materials;
+        for (int i = 0; i < mats.Length; i++)
+        {
+            Debug.Log(mats[i].name);
+            if (mats[i].name == outlineAssetName)
+            {
+                outline = mats[i];
+                break;
+            }
+        }
+    }
+
+
     public void ActivateDialogue()
     {
         speechBubble.SetActive(true);
@@ -17,8 +43,9 @@ public class DialoguePopup : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(dialogueShown) { return; }
+        if (dialogueShown) { return; }
         if (!activateWhenNear) { return; }
+
         if ((other.transform.tag == "Player"))
         {
             // Activate popup if is based on trigger
@@ -26,10 +53,28 @@ public class DialoguePopup : MonoBehaviour
         }
     }
 
+    private void OnTriggerStay(Collider other)
+    {
+        if (dialogueShown) { return; };
+
+        if (activateWhenNear) { return; }
+
+        if ((other.transform.tag == "Player"))
+        {
+            outline.SetFloat("_Outline_Thickness", outlineThickness);
+            if (Input.GetKey(KeyCode.E))
+            {
+                ActivateDialogue();
+            }
+        }
+    }
+
+
     IEnumerator DialogueTimer(float time)
     {
         yield return new WaitForSeconds(time);
         dialogueShown = true;
+        outline.SetFloat("_Outline_Thickness", 0);
         speechBubble?.SetActive(false);
     }
 
