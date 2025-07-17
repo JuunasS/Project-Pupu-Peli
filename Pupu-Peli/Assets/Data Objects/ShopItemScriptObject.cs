cd5daab498f0d5f534779667184f87e8a1641ca9
JoonasS <62592089+JuunasS@users.noreply.github.com>
2025-07-17 12:54:38 +0300

    Shop changes

--- Pupu-Peli/Assets/Data Objects/ShopItemScriptObject.cs
+++ Pupu-Peli/Assets/Data Objects/ShopItemScriptObject.cs
@@ -6,6 +6,7 @@ public class ShopItemScriptObject : ScriptableObject
     public string ID; // This is used for identifying this data object from files and lists
     //public string name;
     [TextArea] public string description;
+    public int price;
 
     // Image should be gotten from assets with in code as string so that it works event when saved to file?
     public Sprite image;
