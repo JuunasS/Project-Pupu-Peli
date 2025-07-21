b4f51dfd731901e5981c0293b2f6961744e2866d
JoonasS <62592089+JuunasS@users.noreply.github.com>
2025-07-21 15:54:59 +0300

    Location variable to ico script object

--- Pupu-Peli/Assets/Data Objects/IcoScriptObject.cs
+++ Pupu-Peli/Assets/Data Objects/IcoScriptObject.cs
@@ -1,14 +1,19 @@
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "IcoScriptObject", menuName = "Scriptable Objects/IcoScriptObject")]
+
 public class IcoScriptObject : ScriptableObject
 {
     public string icoName;
     public Sprite icoImg;
     [TextArea] public string icoText;
+
+    [SerializeField]
+    public Location icoLocation;
 
 
 
@@ -25,4 +30,25 @@ public class IcoScriptObject : ScriptableObject
     public int minProductivity;
     public int maxProductivity;
 
+}
+
+[Serializable]
+public enum Locations
+{
+    Albium,
+    Gorrha,
+    Knoc,
+    Vshoosh,
+    Ckbolcheivvtgnoolho
+}
+
+[Serializable]
+public class Location 
+{
+
+    [SerializeField]
+    public Locations location;
+
+    [SerializeField]
+    public List<string> places;
 }
