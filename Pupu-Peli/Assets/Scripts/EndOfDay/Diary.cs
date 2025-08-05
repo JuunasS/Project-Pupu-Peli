aa87a79ec281c24930179fc0ad02e52bfca57298
JoonasS <62592089+JuunasS@users.noreply.github.com>
2025-08-05 16:14:23 +0300

    Diary

--- Pupu-Peli/Assets/Scripts/EndOfDay/Diary.cs
+++ Pupu-Peli/Assets/Scripts/EndOfDay/Diary.cs
@@ -1,29 +1,73 @@
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Diary : MonoBehaviour
 {
     [SerializeField]
     public List<DayData> daysCompleted;
 
+    public GameObject daySelectButtonPrefab;
+    public Transform buttonPositionZero;
+    public float buttonOffsetY;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject scrollContent;
+
+    // Stats panel UI
+    public TMP_Text dayNumberText;
+    public TMP_Text coinsText;
+    public TMP_Text missionsText;
+    public TMP_Text brewsText;
+    public TMP_Text herbsText;
+
+    private void OnEnable()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        GenerateDaySelectButtons();
+        ShowDayData(0);
     }
 
     public void AddDayData(int dayNum, int coins, string missions, string brews, string herbs)
     {
         daysCompleted.Add(new DayData(dayNum, coins, missions, brews, herbs));
+    }
+
+    public void GenerateDaySelectButtons()
+    {
+        int col = 0;
+        for (int i = 0; i < daysCompleted.Count; i++)
+        {
+            GameObject tempButton = Instantiate(daySelectButtonPrefab);
+
+            tempButton.transform.SetParent(scrollContent.transform, false);
+
+            tempButton.transform.localPosition = buttonPositionZero.localPosition + new Vector3(0, (-buttonOffsetY * col) - daySelectButtonPrefab.GetComponent<RectTransform>().sizeDelta.y * col);
+
+            int tempIndex = i;
+            tempButton.GetComponentInChildren<TMP_Text>().text = "Day " + (i+1);
+            tempButton.GetComponent<Button>().onClick.AddListener(delegate { ShowDayData(tempIndex); });
+
+            col++;
+        }
+
+        RectTransform temp = scrollContent.GetComponent<RectTransform>();
+        temp.sizeDelta = new Vector2(temp.sizeDelta.x, temp.sizeDelta.y + daysCompleted.Count * daySelectButtonPrefab.GetComponent<RectTransform>().sizeDelta.y + daysCompleted.Count * buttonOffsetY);
+    }
+
+    public void ShowDayData(int i)
+    {
+        if (daysCompleted.Count != 0)
+        {
+            Debug.Log("Button " + i + " pressed!");
+            // Set data into stats panel
+            dayNumberText.text = "Day " + daysCompleted[i].dayNumber.ToString();
+            coinsText.text = "Coins Collected: " + daysCompleted[i].coinsCollected.ToString();
+            missionsText.text = daysCompleted[i].missionsCompleted;
+            brewsText.text = daysCompleted[i].brewsCompleted;
+            herbsText.text = daysCompleted[i].herbsCompleted;
+        }
     }
 }
 
